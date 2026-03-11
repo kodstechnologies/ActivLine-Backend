c8ada261d355aa07f91992a606590dcff99c56e7
SP889900 <satya.prakash.fiem.cse20@teamfuture.in>
2026-03-11 12:38:23 +0530

    super admin plans

--- src/controllers/franchise/groupDetails.controller.js
+++ src/controllers/franchise/groupDetails.controller.js
@@ -2,7 +2,16 @@ import { getGroupDetails } from "../../services/franchise/groupDetails.service.j
 
 export const fetchGroupDetails = async (req, res, next) => {
   try {
-    const data = await getGroupDetails();
+    const { accountId } = req.query;
+
+    if (!accountId) {
+      return res.status(400).json({
+        success: false,
+        message: "accountId is required",
+      });
+    }
+
+    const data = await getGroupDetails(accountId);
 
     return res.status(200).json({
       success: true,
--- src/services/franchise/groupDetails.service.js
+++ src/services/franchise/groupDetails.service.js
@@ -1,5 +1,28 @@
-import activlineClient from "../../external/activline/activline.client.js";
+import axios from "axios";
+import Franchise from "../../models/Franchise/franchise.model.js";
+import activlineConfig from "../../config/Jaze_API/Ticket/activline.config.js";
 
-export const getGroupDetails = async () => {
-  return await activlineClient.get("/get_group_details");
+export const getGroupDetails = async (accountId) => {
+  const franchise = await Franchise.findOne({ accountId });
+
+  if (!franchise) {
+    throw new Error("Franchise not found");
+  }
+
+  const username = franchise.accountName;
+  const password = franchise.apiKey;
+  const basicAuth = Buffer.from(`${username}:${password}`).toString("base64");
+
+  const response = await axios.get(
+    `${activlineConfig.baseURL}/get_group_details`,
+    {
+      timeout: activlineConfig.timeout,
+      headers: {
+        Authorization: `Basic ${basicAuth}`,
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
+  return response.data;
 };
