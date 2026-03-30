7c95fb74c221820403a6a9ec15fa0f4c027e282e
Satya889900 <satyaprakash121122@gmail.com>
2026-03-30 12:21:56 +0530

    Firebase error

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -20,7 +20,7 @@ if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
-
+ 
   console.log("✅ Firebase Admin initialized successfully");
 }
 
