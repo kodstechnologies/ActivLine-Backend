5e59bc7b156c15e65276a4cdea95d9fe1cb9d3ea
Satya889900 <satyaprakash121122@gmail.com>
2026-03-30 10:37:37 +0530

    commit

--- src/routes/admin/customer.routes.js
+++ src/routes/admin/customer.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { getPaymentHistoryByCustomerId } from "../../controllers/payment/razorpay.controller.js";
-import { getCustomerTickets } from "../../controllers/admin/customer.ticket.controller.js";
+import { getCustomerTickets } from "../../controllers/Admin/customer.ticket.controller.js";
 
 const router = Router();
 
