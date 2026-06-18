7ed594bc0440f11268492ec85fc96575f82f5746
Umesh Kumar <uk3993907@gmail.com>
2026-06-18 11:18:00 +0530

    Fix: the sms api key

--- src/utils/sendMessage.js
+++ src/utils/sendMessage.js
@@ -1,20 +1,12 @@
 import axios from "axios";
 export const sendMessage = async ({ mobile, message, template_id }) => {
   try {
-    console.log(
-      "Sending message to:",
-      mobile,
-      "with template ID:",
-      template_id,
-    );
     const sender_id = process.env.SMS_SENDER_ID;
     const username = process.env.SMS_USERNAME;
     const password = process.env.SMS_PASSWORD;
     const smsApiUrl = `https://www.smsjust.com/blank/sms/user/urlsms.php?username=${username}&pass=${password}&senderid=${sender_id}&dest_mobileno=${mobile}&message=${encodeURIComponent(message)}&dlttempid=${template_id}&response=Y`;
     const response = await axios.post(smsApiUrl);
-
-    console.log("sms response", response?.data);
   } catch (error) {
-    console.log(error.response?.data || error.message);
+    console.log(error.response?.data);
   }
 };
