9b9bf806c14dd20883f66a63a3eccac6fb3de309
Umesh Kumar <uk3993907@gmail.com>
2026-06-22 14:07:45 +0530

    increase file size

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -7,4 +7,9 @@ const storage = multer.diskStorage({
   },
 });
 
-export const upload = multer({ storage });
+export const upload = multer({
+  storage,
+  limits: {
+    fileSize: 50 * 1024 * 1024, // 50MB
+  },
+});
